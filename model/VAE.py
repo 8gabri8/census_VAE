dfff10f471f4e2a1dd77097fc25c4421f57a05a1
Gabriele Dall'aglio <dalai@jed.jed.cluster>
2024-12-02 22:40:28 +0100

    zscore normalization before merging

--- model/VAE.py
+++ model/VAE.py
@@ -53,7 +53,7 @@ class VAE(AutoEncoder):
     def __init__(self, input_size, latent_dim=256):
         super().__init__(input_size, latent_dim)
         
-        # Define mu and log_var for reparameterization trick
+        # mu and log_var are simpleNN that will meant the mean and std from the latent space emb
         self.mu = nn.Linear(latent_dim, latent_dim)
         self.log_var = nn.Linear(latent_dim, latent_dim)
 
@@ -83,7 +83,7 @@ class VAE(AutoEncoder):
     def sample(self, num_samples):
         with torch.no_grad():
             # Sample from the standard normal distribution
-            z = torch.randn(num_samples, self.latent_dim).to(next(self.parameters()).device)
+            z = torch.randn(num_samples, self.latent_dim).to(next(self.parameters()).device) # put z on device of the mdoel
             # Pass the latent samples through the decoder
             samples = self.decoder(z)
         return samples
@@ -92,12 +92,15 @@ class VAEWithClassifier(VAE):
     def __init__(self, input_size, latent_dim=256, num_classes=10):
         super().__init__(input_size, latent_dim)
         
-        # Define the classification head
+        # Define the classification NN
         self.classifier = nn.Sequential(
             nn.Linear(latent_dim, 128),
             nn.ReLU(),
             nn.Linear(128, num_classes)  # Output layer for classification
         )
+
+        # Could create also two different classifiers, one for cell_type an one for disease
+        # TODO:
         
     def forward(self, x):
         # Pass the input through the encoder
@@ -106,6 +109,9 @@ class VAEWithClassifier(VAE):
         # Compute the mean and log variance for the VAE
         mu = self.mu(encoded)
         log_var = self.log_var(encoded)
+
+        # Attention:clipping
+        log_var = torch.clamp(log_var, min=-5, max=5)
         
         # Reparameterize the latent vector
         z = self.reparameterize(mu, log_var)
@@ -114,14 +120,44 @@ class VAEWithClassifier(VAE):
         decoded = self.decoder(z)
         
         # Classifier: Classify the latent space representation
-        classification_output = self.classifier(encoded)
+        logits_classification = self.classifier(encoded)
         
-        return encoded, decoded, mu, log_var, classification_output
+        return encoded, decoded, mu, log_var, logits_classification
+    
+    def predict_from_logit(self, logits_classification):
 
-def loss_BCE_KLD_classification(recon_x, x, mu, logvar, classification_output, labels,
-                                 weight_BCE=1.0, weight_KLD=1.0, weight_classification_loss=1.0):
+        # Apply softmax to get probabilities
+        probabilities = F.softmax(logits_classification, dim=1)
+        
+        # Get the predicted class label (index of the max probability)
+        predicted_classes = torch.argmax(probabilities, dim=1)
+        
+        return predicted_classes
+    
+    def predict_from_input(self, input):
+
+        _, _, _, _, logits_classification = self.forward(input)
+        
+        return self.predict_from_logit(logits_classification)
+
+
+def losses_VAE(
+    recon_x, 
+    x, 
+    mu, 
+    logvar, 
+    classification_output, 
+    labels,
+    reconstruction_loss = "mse",
+    weight_reconstruction=1.0, 
+    weight_KLD=1.0, 
+    weight_classification_loss=1.0
+):
     """
-    Compute the combined loss for the VAE with classification: Reconstruction loss + KL divergence + Classification loss.
+    Compute the combined losses for the VAE with classification: 
+        - Reconstruction loss --> MSE
+        - Normality fo latent space --> KL divergence 
+        - Classification loss --> Cross Entropy
     
     Arguments:
     recon_x (tensor): The reconstructed input from the decoder.
@@ -130,72 +166,115 @@ def loss_BCE_KLD_classification(recon_x, x, mu, logvar, classification_output, l
     logvar (tensor): The log variance of the latent variable distribution from the encoder.
     classification_output (tensor): The classifier output.
     labels (tensor): The true labels for classification.
-    weight_BCE (float): Weight for the reconstruction loss.
-    weight_KLD (float): Weight for the KL divergence loss.
-    weight_classification_loss (float): Weight for the classification loss.
     
     Returns:
     total_loss (tensor): The combined, weighted loss value (normalized to 1).
     """
+
+    # Reconstruction Loss
+    if reconstruction_loss == "mse":
+        mse_loss_fn = nn.MSELoss(reduction='mean')
+        rec_loss = mse_loss_fn(recon_x, x) # Calculte the mean of each single sample loss
+    elif reconstruction_loss == "bce":
+        rec_loss = F.binary_cross_entropy(recon_x, x, reduction="sum")
     
-    # VAE losses
-    BCE = F.binary_cross_entropy(recon_x, x, reduction="sum")  # sum of BCE for all features
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())  # KL divergence
-    
-    # Classification loss
-    classification_loss = F.cross_entropy(classification_output, labels)
+    # Normality for latent space --> KL divergence 
+    #KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / mu.size(0)
+
+
+    # Classification loss --> Cross Entropy
+    classification_loss = F.cross_entropy(classification_output, labels, reduction='mean')
     
     # Weighted losses
-    BCE_weighted = weight_BCE * BCE
+    rec_loss_weighted = weight_reconstruction * rec_loss
     KLD_weighted = weight_KLD * KLD
     classification_loss_weighted = weight_classification_loss * classification_loss
     
     # Total loss (sum of weighted losses)
-    total_loss = BCE_weighted + KLD_weighted + classification_loss_weighted
+    total_loss = rec_loss_weighted + KLD_weighted + classification_loss_weighted
     
     # Normalize the total loss to be 1 (optional)
-    total_loss = total_loss / total_loss.item()  # Normalize by the final loss value
+    #total_loss = total_loss / total_loss.item()  # Normalize by the final loss value
     
-    # Return individual losses
-    return total_loss, BCE_weighted, KLD_weighted, classification_loss_weighted
+    print(f"Total Loss: {total_loss.item():.4f} | "
+        f"Reconstruction Loss (weighted): {rec_loss_weighted.item():.4f} | "
+        f"KLD (weighted): {KLD_weighted.item():.4f} | "
+        f"Classification Loss (weighted): {classification_loss_weighted.item():.4f}")
+    
+    return total_loss, rec_loss_weighted, KLD_weighted, classification_loss_weighted
 
 
-def train_VAE_with_classification(model, train_loader, lr=1e-3, num_epochs=10):
-    """
-    Train a Variational Autoencoder (VAE) with a classification head.
+def train_VAE_with_classification(
+    model, 
+    adata,
+    fold_indices, 
+    lr=1e-5, 
+    num_epochs=5,
+    batch_size=64,
+    weigth_losses = [1, 0.0001, 1] #reconstruction, kl, classification
+):
     
-    Arguments:
-    model: The VAE with classification model.
-    train_loader: The training data loader.
-    lr: Learning rate.
-    num_epochs: Number of epochs for training.
-    """
-    
+    # Extarct weigth of losses
+    w_rec = weigth_losses[0]
+    w_kl = weigth_losses[1]
+    w_cls = weigth_losses[2]
+
     # Automatically detect the device of the model
     device = next(model.parameters()).device
     
     # Initialize optimizer
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
+    # Indices of cells in train and test
+    indices_train = fold_indices[0] # ATTENTION: the indices are in random order, not in increasing order
+    indices_test = fold_indices[1]
+    num_samples_train = len(indices_train)
+    num_samples_test = len(indices_test)
+    print(f"num_samples_train: {num_samples_train}, num_samples_test: {num_samples_test}")
+
+    # Save lists
+    epoch_losses_train = []
+    epoch_losses_test = []
+
     # Training loop
     for epoch in range(num_epochs):
+
         model.train()
         total_loss = 0.0  # Initialize total loss for the epoch
 
+        # Create list with batch indices
+        batch_indices_train = [indices_train[i:i + batch_size] for i in range(0, len(indices_train), batch_size)]
+        num_batches_train = len(batch_indices_train)
+        print(f"\nnum_batches_train: {num_batches_train}")
+
         # Iterate over the batches in the training data
-        for data, labels in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{num_epochs}", unit="batch"):
+        for single_batch_indices_train in tqdm(batch_indices_train, desc=f"Epoch {epoch + 1}/{num_epochs}", unit="batch"):
+            
             optimizer.zero_grad()
 
+            # Ectract data and labels of this batch
+            adata_tmp = adata[single_batch_indices_train, ]
+            y = adata_tmp.obs["concat_label_encoded"].values.tolist()
+            x = adata_tmp.X.toarray() #num_cells(batch_size) x num_genes
+
             # Move data and labels to the appropriate device
-            data, labels = data.to(device), labels.to(device)
+            x = torch.tensor(x, dtype=torch.float32).to(device)
+            y = torch.tensor(y, dtype=torch.long).to(device)
+            print(f"x shape: {x.shape}, y shape: {y.shape}")
 
             # Forward pass
-            encoded, decoded, mu, log_var, classification_output = model(data)
+            encoded, decoded, mu, log_var, logits_classification = model(x)
+
+            # Print mu and log_var with detailed formatting
+            print(f"Mu: {mu.detach().cpu().numpy() if mu.is_cuda else mu.detach().numpy()} | "
+                f"\nLog Variance (log_var): {log_var.detach().cpu().numpy() if log_var.is_cuda else log_var.detach().numpy()}")
+
 
             # Compute the loss with weights for each component
-            loss, BCE_weighted, KLD_weighted, classification_loss_weighted = loss_BCE_KLD_classification(
-                decoded, data, mu, log_var, classification_output, labels,
-                weight_BCE=1.0, weight_KLD=1.0, weight_classification_loss=1.0
+            loss, BCE_weighted, KLD_weighted, classification_loss_weighted = losses_VAE(
+                decoded, x, mu, log_var, logits_classification, y,
+                weight_reconstruction=w_rec, weight_KLD=w_kl, weight_classification_loss=w_cls
             )
 
             # Backward pass
@@ -205,76 +284,97 @@ def train_VAE_with_classification(model, train_loader, lr=1e-3, num_epochs=10):
             # Accumulate the total loss
             total_loss += loss.item()
 
-            # Print out the individual losses (optional)
-            # It's better to print periodically, not on every batch
-            if (epoch + 1) % 5 == 0:  # Print every 5 epochs or adjust as needed
-                print(f"Batch Losses -> BCE: {BCE_weighted:.4f}, KLD: {KLD_weighted:.4f}, Classification Loss: {classification_loss_weighted:.4f}, Total Loss: {loss:.4f}")
+            del x, y, adata_tmp
+            torch.cuda.empty_cache()
         
         # Compute the average loss for this epoch
-        epoch_loss = total_loss / len(train_loader.dataset)  # Total loss / number of examples
-        print(f"Epoch {epoch + 1}/{num_epochs}: Average Loss = {epoch_loss:.4f}")
-    
-    # Return the trained model
-    return model
+        epoch_loss_train = total_loss / num_samples_train # Total loss / number of examples
+        epoch_losses_train.append(epoch_loss_train)        
 
+        #####################
+        # Evaluation
 
+        with torch.no_grad():
 
+            model.eval()
+            total_loss = 0.0  # Initialize total loss for the epoch
 
-# def train_VAE(model, train_loader, lr=1e-3, num_epochs=10):
-#     """
-#     Train a Variational Autoencoder (VAE) model.
-    
-#     Arguments:
-#     model: The VAE model.
-#     train_loader: The training data loader.
-#     lr: Learning rate.
-#     num_epochs: Number of epochs for training.
-#     """
-    
-#     # Automatically detect the device of the model
-#     device = next(model.parameters()).device
-    
-#     # Initialize optimizer
-#     optimizer = optim.Adam(model.parameters(), lr=lr)
+            # Create list with batch indices
+            batch_indices_test = [indices_test[i:i + batch_size] for i in range(0, len(indices_test), batch_size)]
+            num_batches_test = len(batch_indices_test)
 
-#     # Define the loss function
-#     criterion = nn.MSELoss()
+            # Iterate over the batches in the training data
+            for single_batch_indices_test in tqdm(batch_indices_test, desc=f"Epoch {epoch + 1}/{num_epochs}", unit="batch"):
+                
+                # Ectract data and labels of this batch
+                adata_tmp = adata[single_batch_indices_test, ]
+                y = adata_tmp.obs["concat_label_encoded"].values.tolist()
+                x = adata_tmp.X.toarray() #num_cells(batch_size) x num_genes
 
-#     # Training loop
-#     for epoch in num_epochs:
-        
-#         model.train()
+                # Move data and labels to the appropriate device
+                x = torch.tensor(x, dtype=torch.float32).to(device)
+                y = torch.tensor(y, dtype=torch.long).to(device)
 
-#         total_loss = 0.0
+                # Forward pass
+                encoded, decoded, mu, log_var, logits_classification = model(x)
 
-#         # Iterate over the batches in the training data
-#         for data in tqdm(train_loader, desc="Batch", unit="batch"):
+                # Compute the loss with weights for each component
+                loss, BCE_weighted, KLD_weighted, classification_loss_weighted = losses_VAE(
+                    decoded, x, mu, log_var, logits_classification, y,
+                    weight_reconstruction=w_rec, weight_KLD=w_kl, weight_classification_loss=w_cls
+                )
+
+                # Accumulate the total loss
+                total_loss += loss.item()
+
+                del x, y, adata_tmp
+                torch.cuda.empty_cache()
             
-#             optimizer.zero_grad()
+            # Compute the average loss for this epoch
+            epoch_loss_test = total_loss / num_samples_test # Total loss / number of examples
+            epoch_losses_test.append(epoch_loss_test)  
 
-#             # Get a batch of training data and move it to the device
-#             data = data.to(device)
-
-#             # Forward pass
-#             encoded, decoded, mu, log_var = model(data)
-
-#             # Compute the loss
-#             loss_reconstruction = criterion(decoded, data)
-#             loss_kld = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-
-#             loss = loss_reconstruction + loss_kld
-
-#             # Backward pass and optimization
-#             loss.backward()
-#             optimizer.step()
-
-#             # Update the running total loss
-#             total_loss += loss.item() * data.size(0)  # Sum the batch losses
-
-#         # Compute the average loss for this epoch
-#         epoch_loss = total_loss / len(train_loader.dataset)
-#         print(f"Epoch {epoch + 1}/{num_epochs}: Loss = {epoch_loss:.4f}")
+            print(f"Train Loss: {epoch_loss_train:.2f}, Test Loss: {epoch_loss_test:.2f}")
 
 
-#     # Return the trained model
-#     return model
+    # Return the trained model
+    return model, epoch_losses_train, epoch_losses_test
+
+
+
+# TODO: Conditional VAE
+# class ConditionalVAE(VAE):
+#     # VAE implementation from the article linked above
+#     def __init__(self, num_classes):
+#         super().__init__()
+#         # Add a linear layer for the class label
+#         self.label_projector = nn.Sequential(
+#             nn.Linear(num_classes, self.num_hidden),
+#             nn.ReLU(),
+#         )
+
+#     def condition_on_label(self, z, y):
+#         projected_label = self.label_projector(y.float())
+#         return z + projected_label
+
+#     def forward(self, x, y):
+#         # Pass the input through the encoder
+#         encoded = self.encoder(x)
+#         # Compute the mean and log variance vectors
+#         mu = self.mu(encoded)
+#         log_var = self.log_var(encoded)
+#         # Reparameterize the latent variable
+#         z = self.reparameterize(mu, log_var)
+#         # Pass the latent variable through the decoder
+#         decoded = self.decoder(self.condition_on_label(z, y))
+#         # Return the encoded output, decoded output, mean, and log variance
+#         return encoded, decoded, mu, log_var
+
+#     def sample(self, num_samples, y):
+#         with torch.no_grad():
+#             # Generate random noise
+#             z = torch.randn(num_samples, self.num_hidden).to(device)
+#             # Pass the noise through the decoder to generate samples
+#             samples = self.decoder(self.condition_on_label(z, y))
+#         # Return the generated samples
+#         return samples
--- model/train.py
+++ model/train.py
@@ -1,20 +1,131 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+import torch
+import scanpy as sc
+import matplotlib.pyplot as plt
 
 
-# Load h5ad
+from VAE import *
 
-# Exctract matrix
+#########################
+### Setting Device
+#########################
 
-# Put to dense
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
 
-# Split in train and test
+#########################
+### Set Random Seed
+#########################
 
-# Create dataloader
-train_loader = torch.utils.data.DataLoader(
-    X_train, batch_size=batch_size, shuffle=True
+np.random.seed(42)
+torch.manual_seed(42)
+
+#########################
+### Load h5ad
+#########################
+
+adata = sc.read("/work/upcourtine/clock-classifier/gabriele-results/census_results/merged_30.h5ad")
+print(type(adata.X))
+adata = adata[:100,]
+adata.write("/work/upcourtine/clock-classifier/gabriele-results/census_results/merged_30_100_cells.h5ad")
+#adata = sc.read("/work/upcourtine/clock-classifier/gabriele-results/census_results/merged_30_100_cells.h5ad")
+# Number of cells
+n_cells = adata.obs.shape[0]
+# Number of Genes
+n_genes = adata.var.shape[0]
+# Number fo classes to predict
+n_classes = len(adata.obs["concat_label_encoded"].unique())
+n_classes = 265 # ATTENTION: only when using smaller dataset, otherwise CrossEntirpy will give error
+# Number of Epcohs
+num_epochs = 5
+
+print(f"n_cells: {n_cells}, n_genes: {n_genes}, n_classes: {n_classes}")
+
+# Extraction of values(single cell expression + labels) will be done isde the train loop, in order to put in a dense format only the current batch
+
+#########################
+### Implement Cross Validation
+#########################
+
+# Number of folds 
+n_folds = 5
+
+# Initialize a list to store the indices for different splits
+splits_indices = []
+
+# Set the number of splits you want, here we'll use a single train/test split
+for _ in range(n_folds):
+    # Perform train/test split
+    train_indices, val_indices = train_test_split(range(n_cells), test_size=0.2, random_state=None)
+    
+    # Store the indices for each split in the list
+    splits_indices.append((train_indices, val_indices))
+
+print(f"Total number of Folds: {len(splits_indices)}")
+train_indices, val_indices = splits_indices[0]
+print(f"Training indices shape: {len(train_indices)}")
+print(f"Validation indices shape: {len(val_indices)}")
+
+#########################
+### Instantiate the model
+#########################
+
+model = VAEWithClassifier(
+    input_size = n_genes,
+    latent_dim=256, 
+    num_classes=n_classes
 )
-# one entity is one cells with its genes values
 
-# Instantiate model
-model = AutoEncoder()
 model.to(device)
+
+# Attention
+torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
+#########################
+### Train
+#########################
+
+models = [] # each elemtn is a trained model from fold
+losses_train = []
+losses_test = [] # each element is a list with the losses of a fold
+
+# Perform Cross Validation
+for i, fold in enumerate(splits_indices):
+
+    print(f"\nCross Validation, fold {i+1}/{n_folds}")
+
+    # Instantiate the model --> reinitialize each time
+    model = VAEWithClassifier(
+        input_size = n_genes,
+        latent_dim=256, 
+        num_classes=n_classes
+    )
+    model.to(device)
+
+    # Call Training loop
+    model, epoch_losses_train, epoch_losses_test = train_VAE_with_classification(
+        model = model, 
+        fold_indices = fold, 
+        lr=1e-3, 
+        num_epochs=num_epochs,
+        adata = adata,
+        batch_size=10
+    )
+
+    models.append(model)
+    losses_train.append(epoch_losses_train)
+    losses_test.append(epoch_losses_test)
+
+    # Save image
+    plt.figure(figsize=(10, 6))
+    plt.plot(epoch_losses_train, label='Training Loss', color='blue', marker='o')
+    plt.plot(epoch_losses_test, label='Testing Loss', color='red', marker='o')
+    plt.title('Training and Testing Loss over Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    image_path = f'/scratch/dalai/census_VAE/log/{i}_losses_plot.png'
+    plt.savefig(image_path)
 
